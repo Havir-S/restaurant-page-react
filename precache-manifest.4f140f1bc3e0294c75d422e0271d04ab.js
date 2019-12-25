e18088a5e21e5cda8bcfc6fa4321a7012bbc12a8
Havir-S <Havir-S@users.noreply.github.com>
2019-12-25 19:34:20 +0100

    Updates

---
+++ precache-manifest.4f140f1bc3e0294c75d422e0271d04ab.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f10d34ebd75a51c71f99422f4786a1",
+    "url": "/restaurant-page-react/index.html"
+  },
+  {
+    "revision": "f698d11d6f9fa77fd086",
+    "url": "/restaurant-page-react/static/css/main.c5cdc22d.chunk.css"
+  },
+  {
+    "revision": "c84438f9af146812c93f",
+    "url": "/restaurant-page-react/static/js/2.bf01ab36.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/restaurant-page-react/static/js/2.bf01ab36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f698d11d6f9fa77fd086",
+    "url": "/restaurant-page-react/static/js/main.366c066d.chunk.js"
+  },
+  {
+    "revision": "926edbdfebcd03b4de28",
+    "url": "/restaurant-page-react/static/js/runtime-main.f27f2b7b.js"
+  },
+  {
+    "revision": "dd9c19a4542dfa01563adb0025a26787",
+    "url": "/restaurant-page-react/static/media/brickwall6.dd9c19a4.png"
+  },
+  {
+    "revision": "b86ac8d95cc828f3bf01a84fac9ee6f6",
+    "url": "/restaurant-page-react/static/media/cartographer.b86ac8d9.png"
+  },
+  {
+    "revision": "d96467f34b5e310c5b93863fd74d29ec",
+    "url": "/restaurant-page-react/static/media/facebook.d96467f3.svg"
+  },
+  {
+    "revision": "68c297954def64495ecfd42aafeac87e",
+    "url": "/restaurant-page-react/static/media/pillartest332.68c29795.png"
+  },
+  {
+    "revision": "42950d5e87c004490f9fc89c78eb2f91",
+    "url": "/restaurant-page-react/static/media/pinterest.42950d5e.svg"
+  },
+  {
+    "revision": "44da77b9c087263105be6ef475fcd980",
+    "url": "/restaurant-page-react/static/media/pizza1.44da77b9.jpg"
+  },
+  {
+    "revision": "5ddbbd743b3a75975eebd642e14ca23b",
+    "url": "/restaurant-page-react/static/media/restaurant1.5ddbbd74.jpg"
+  },
+  {
+    "revision": "47dfbbee6245322a22cefe822634cc29",
+    "url": "/restaurant-page-react/static/media/spaghetti.47dfbbee.jpg"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/restaurant-page-react/static/media/twitter.7e145c9e.svg"
+  },
+  {
+    "revision": "22fc0aa7ebdfcd7a923bbe3c97aba7f2",
+    "url": "/restaurant-page-react/static/media/wall4.22fc0aa7.png"
+  },
+  {
+    "revision": "a25f1231bc89d249c8a7bcd7e6619de7",
+    "url": "/restaurant-page-react/static/media/wall5.a25f1231.png"
+  },
+  {
+    "revision": "1d606d5f628e14d0f6a21bef91fb5164",
+    "url": "/restaurant-page-react/static/media/wall5_down4.1d606d5f.png"
+  },
+  {
+    "revision": "8968a4b8a454b9158ff4b5e6040f1672",
+    "url": "/restaurant-page-react/static/media/youtube.8968a4b8.svg"
+  }
+]);
